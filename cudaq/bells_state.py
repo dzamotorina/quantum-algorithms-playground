eef7dd7a3dac064eade7c92c32ee412ba4ca350c
Daria Zamotorina <dzamotorina@gmail.com>
2026-08-10 22:48:23 -0500

    improve the code by adding args to the function

--- cudaq/bells_state.py
+++ cudaq/bells_state.py
@@ -1,18 +1,19 @@
-import sys
 import cudaq
 
-print(f"Running on target {cudaq.get_target().name}")
-qubit_count = int(sys.argv[1]) if 1 < len(sys.argv) else 2
 
-
+# Define our kernel.
 @cudaq.kernel
-def kernel():
-    qubits = cudaq.qvector(qubit_count)
-    h(qubits[0])
-    for i in range(1, qubit_count):
-        x.ctrl(qubits[0], qubits[i])
-    mz(qubits)
+def kernel(qubit_count: int):
+    # Allocate our qubits.
+    qvector = cudaq.qvector(qubit_count)
+    # Place the first qubit in the superposition state.
+    h(qvector[0])
+    # Loop through the allocated qubits and apply controlled-X,
+    # or CNOT, operations between them.
+    for qubit in range(qubit_count - 1):
+        x.ctrl(qvector[qubit], qvector[qubit + 1])
+    # Measure the qubits.
+    mz(qvector)
 
-
-result = cudaq.sample(kernel)
-print(result)  # Example: { 11:500 00:500 }
+result = cudaq.sample(kernel, 3)
+print(result)
